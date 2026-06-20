d5ccb068791d113673f7a9c68c9119a6b6eec54a
DHM040702 <diegoh04m@gmail.com>
2026-06-20 00:41:23 -0500

    Version 16.0

--- Iniciar-servidores-WSL.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-:: CMD 1 - Backend
-start cmd /k "wsl -u cpu_ux bash /mnt/c/sga-academia/backend.sh"
-
-:: CMD 2 - Frontend
-start cmd /k "wsl -u cpu_ux bash /mnt/c/sga-academia/frontend.sh"
